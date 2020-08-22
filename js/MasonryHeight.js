e49e2ef5b8b4c06aa6c6f2fe7a06dd0d6b13a096
LogCreative <lizilong123456@live.cn>
2020-08-23 00:40:47 +0800

    SVG Scaling Onload

--- js/MasonryHeight.js
+++ js/MasonryHeight.js
@@ -11,26 +11,29 @@ $(document).ready(function() {
             SingleHeight += $(this).height();
             $(this).find('.mcontent .svgimg embed').each(function() {
                 //寻找 svg 标签
-                var d = $(this).getSVGDocument();
-                var svg = d.querySelector('svg');
-                console.log(svg);
-                
-                if(svg.getAttribute('width')>itemWidth){
-                    //需要缩放
-                    svg.setAttribute("preserveAspectRatio","xMinYMin meet");
-                    svg.style.overflow="hidden";
-                    var viewBoxVal = svg.getAttribute("viewBox");
-                    if(viewBoxVal){
-                        var viewBoxWidth = viewBoxVal.split(" ")[2];
-                        var viewBoxHeight = viewBoxVal.split(" ")[3];
-                        svg.removeAtrribute("width");
-                        svg.removeAtrribute("height");
-        
-                        var setHeight = itemWidth * viewBoxHeight / viewBoxWidth;
-                        svg.setAttribute("width", itemWidth);
-                        svg.setAttribute("height", setHeight);
+                $(this).onload = function() {
+                    var d = $(this).getSVGDocument();
+                    var svg = d.querySelector('svg');
+                    console.log(svg);
+                    
+                    if(svg.getAttribute('width')>itemWidth){
+                        //需要缩放
+                        svg.setAttribute("preserveAspectRatio","xMinYMin meet");
+                        svg.style.overflow="hidden";
+                        var viewBoxVal = svg.getAttribute("viewBox");
+                        if(viewBoxVal){
+                            var viewBoxWidth = viewBoxVal.split(" ")[2];
+                            var viewBoxHeight = viewBoxVal.split(" ")[3];
+                            svg.removeAtrribute("width");
+                            svg.removeAtrribute("height");
+            
+                            var setHeight = itemWidth * viewBoxHeight / viewBoxWidth;
+                            svg.setAttribute("width", itemWidth);
+                            svg.setAttribute("height", setHeight);
+                        }
                     }
                 }
+                
             });
         });
         if(document.documentElement.clientWidth<=1000){ 
