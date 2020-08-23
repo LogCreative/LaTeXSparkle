97e3cab065aa7e3c8f15185e139a38ce3ddb8650
LogCreative <lizilong123456@live.cn>
2020-08-23 14:53:47 +0800

    Mobile Masonry Alg.

--- js/MasonryHeight.js
+++ js/MasonryHeight.js
@@ -1,26 +1,31 @@
 $(document).ready(function() {
 
+    var max = function(a,b){
+        return (a>b?a:b);
+    };
    
-
     // 计算瀑布流高度
     var calcHeight = function(){
-        var SingleHeight = 0;
-
-        $('.masonry').find('.mitem').each(function() {
-            // console.log($(this));
-            // var ord = $(this).getAttribute('order');
-            // console.log(ord);
-            SingleHeight += $(this).height();
-        });
+        var resultHeight = 0;
+        var count = 1;
         if(document.documentElement.clientWidth<=1000){ 
-            //Mobile
-            SingleHeight = SingleHeight /1.8; 
+            //Mobile 2 Columns
+            var Heights = [0,0];
+            $('.masonry').find('.mitem').each(function(){
+                Heights[(count++) % 2] += $(this).height();
+            });
+            resultHeight = max(Heights[0],Heights[1]) + count / 2 * 10;
+            //SingleHeight = SingleHeight /1.8; 
         } 
         else {
-            //Desktop
-            SingleHeight = SingleHeight / 2.75;
+            //Desktop 3 Columns
+            var SingleHeight = 0;
+            $('.masonry').find('.mitem').each(function(){
+                SingleHeight += $(this).height();
+            });
+            resultHeight = SingleHeight / 2.8 + 100;
         }
-        $('.masonry').css('height', SingleHeight);
+        $('.masonry').css('height', resultHeight);
 
     };
 
